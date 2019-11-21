bcdc1a3f5a87db92e609a069162f7bdb43e9db0c
tryToCode <dong.yang@campus.tu-berlin.de>
2019-11-21 09:18:23 +0100

    add requirements.txt

--- podcast/api/views.py
+++ podcast/api/views.py
@@ -4,6 +4,7 @@ from podcast.api.serializers import UserSerializer, RssItemSerializer, PodcastSe
 from .models import RssItem, Podcast
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from datetime import date
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -23,14 +24,33 @@ class RssItemViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows rss feed items to be viewed or edited.
     """
+    cat_queryKey_mapper = {
+        'IT': 1,
+        'Entrepreneurship': 2,
+        'Finance': 3
+    }
+
     serializer_class = RssItemSerializer
     def get_queryset(self):
         """    
         Optionally restricts the returned items to a given category,
         by filtering against a `category` query parameter in the URL.
         """
-        queryset = RssItem.objects.all().order_by('-pub_date')
+        queryset = RssItem.objects.all().order_by('pub_date')
         category = self.request.query_params.get('category', None)
         if category is not None:
-            queryset = queryset.filter(creator__category=category)
+            queryKey = self.cat_queryKey_mapper[category]
+            queryset = queryset.filter(creator__category=queryKey)
+
+        query_date = self.request.query_params.get('date', None)
+        if query_date is not None:
+            today = date.today()
+            if date == 'Last 24':
+                queryset = queryset.filter(pub_date__day=today.day)
+            elif date == 'Past Week':
+                queryset = queryset.filter(pub_date__week=today.week)
+            elif date == 'Past Month':
+                queryset = queryset.filter(pub_date__month=today.month)
+            else:
+                queryset = queryset.filter(pub_date__year=today.year)
         return queryset
--- podcast/urls.py
+++ podcast/urls.py
@@ -25,6 +25,6 @@ router.register(r'rssItems', views.RssItemViewSet, base_name='rssItems')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
